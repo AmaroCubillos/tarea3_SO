d9c85b0e2c5f63b1138f92512f0e1ff462759898
AmaroCubillos <148557542+AmaroCubillos@users.noreply.github.com>
2024-12-27 13:17:44 -0300

    Update tarea3.c

--- tarea3.c
+++ tarea3.c
@@ -1,96 +1,70 @@
+#include "tarea3s.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
 #include <pthread.h>
 #include <semaphore.h>
 
-#define MAX_BUFFER 100  
-typedef struct {
-    int *buffer_harina;
-    int *buffer_carne;
-    int *buffer_vegetales;
-    int *buffer_especias;
+Config config;
+BufferCircular *buffer_harina;
+BufferCircular *buffer_carne;
+BufferCircular *buffer_vegetales;
+BufferCircular *buffer_especias;
+sem_t batidora, sarten;
+int platos_preparados = 0;
+pthread_mutex_t mutex_platos;
 
-    int indice_produccion[4];
-    int indice_consumo[4];
-
-    sem_t mutex_mesa;
-    sem_t espacios_disponibles[4];
-    sem_t ingredientes_disponibles[4];
-} Mesa;
-
-typedef struct {
-    sem_t batidora;
-    sem_t sarten;
-} Utensilios;
-
-Mesa mesa_central;
-Utensilios utensilios;
-int contador_platillos = 0;
-int programa_activo = 1;
-pthread_mutex_t contador_platillos_mutex;
-
-
-int total_platillos;
-int tamano_mesa;
-
-void inicializar_mesa(int tamano) {
-    mesa_central.buffer_harina = (int *)malloc(tamano * sizeof(int));
-    mesa_central.buffer_carne = (int *)malloc(tamano * sizeof(int));
-    mesa_central.buffer_vegetales = (int *)malloc(tamano * sizeof(int));
-    mesa_central.buffer_especias = (int *)malloc(tamano * sizeof(int));
-
-    for (int i = 0; i < 4; i++) {
-        mesa_central.indice_produccion[i] = 0;
-        mesa_central.indice_consumo[i] = 0;
-        sem_init(&mesa_central.espacios_disponibles[i], 0, tamano);
-        sem_init(&mesa_central.ingredientes_disponibles[i], 0, 0);
-    }
-
-    sem_init(&mesa_central.mutex_mesa, 0, 1);
+BufferCircular* crear_buffer(int tamano) {
+    BufferCircular *buffer = malloc(sizeof(BufferCircular));
+    buffer->buffer = calloc(tamano, sizeof(int));
+    buffer->in = 0;
+    buffer->out = 0;
+    sem_init(&buffer->espacios_libres, 0, tamano);
+    sem_init(&buffer->espacios_utilizados, 0, 0);
+    pthread_mutex_init(&buffer->mutex, NULL);
+    return buffer;
 }
 
-void inicializar_utensilios() {
-    sem_init(&utensilios.batidora, 0, 1);
-    sem_init(&utensilios.sarten, 0, 1);
-}
-
-void incrementar_contador_platillos() {
-    pthread_mutex_lock(&contador_platillos_mutex);
-    contador_platillos++;
-    printf("platillo completado. Total: %d\n", contador_platillos);
-    pthread_mutex_unlock(&contador_platillos_mutex);
+void liberar_buffer(BufferCircular *buffer) {
+    free(buffer->buffer);
+    sem_destroy(&buffer->espacios_libres);
+    sem_destroy(&buffer->espacios_utilizados);
+    pthread_mutex_destroy(&buffer->mutex);
+    free(buffer);
 }
 
 void *productor(void *arg) {
-    int tipo_ingrediente = *(int *)arg;
+    ThreadArgs *args = (ThreadArgs *)arg;
+    BufferCircular *buffer = args->buffer;
 
-    while (programa_activo) {
-        sem_wait(&mesa_central.espacios_disponibles[tipo_ingrediente]);
-        sem_wait(&mesa_central.mutex_mesa);
+    while (1) {
+        sem_wait(&buffer->espacios_libres);
+        pthread_mutex_lock(&buffer->mutex);
 
-        if (!programa_activo) {
-            sem_post(&mesa_central.mutex_mesa);
-            sem_post(&mesa_central.espacios_disponibles[tipo_ingrediente]);
+        if (platos_preparados >= config.total_platos) {
+            pthread_mutex_unlock(&buffer->mutex);
+            sem_post(&buffer->espacios_libres);
             break;
         }
 
-        
-        int *buffer;
-        switch (tipo_ingrediente) {
-            case 0: buffer = mesa_central.buffer_harina; break;
-            case 1: buffer = mesa_central.buffer_carne; break;
-            case 2: buffer = mesa_central.buffer_vegetales; break;
-            case 3: buffer = mesa_central.buffer_especias; break;
-        }
+        buffer->buffer[buffer->in] = 1;
+        buffer->in = (buffer->in + 1) % config.tam_buffer;
+        printf("Productor %s %d: produjo ingrediente\n", args->tipo, args->id);
 
-        int idx = mesa_central.indice_produccion[tipo_ingrediente];
-        buffer[idx] = 1; 
-        mesa_central.indice_produccion[tipo_ingrediente] = (idx + 1) % tamano_mesa;
-
-        printf("Productor de ingrediente %d produjo\n", tipo_ingrediente);
-
-        sem_post(&mesa_central.mutex_mesa);
-        sem_post(&mesa_central.ingredientes_disponibles[tipo_ingrediente]);
+        pthread_mutex_unlock(&buffer->mutex);
+        sem_post(&buffer->espacios_utilizados);
     }
     return NULL;
+}
+
+int verificar_termino() {
+    pthread_mutex_lock(&mutex_platos);
+    if (platos_preparados >= config.total_platos) {
+        pthread_mutex_unlock(&mutex_platos);
+        return 1;
+    }
+    platos_preparados++;
+    pthread_mutex_unlock(&mutex_platos);
+    return 0;
 }
